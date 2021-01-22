8fb347e45bd00aeb09d12ec37ba9b4a51b316995
Vu Hung Cuong <cuongvh@gemvietnam.com>
2021-01-22 17:03:10 +0700

    Add id and js

---
+++ script.js
@@ -0,0 +1,33 @@
+let form = document.getElementById("form");
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirmPassword");
+let btn = document.getElementById("btnSubmit");
+
+
+// Show input error message
+
+
+// Show input success message 
+
+
+// Check value input username 
+
+
+// Check value input email
+
+
+// Check value input password
+
+
+// Check value input confirm password
+
+
+// Event 
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+
+});
+
